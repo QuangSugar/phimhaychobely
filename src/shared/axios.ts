290f15fb7c07a471f80a6ffcdceb8f23d36ce77b
Sugar <hnquang.20it9@vku.udn.vn>
2022-05-07 01:41:08 +0700

    update

--- src/shared/axios.ts
+++ src/shared/axios.ts
@@ -2,7 +2,7 @@ import { PROXY } from "./constants";
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `${PROXY}https://ga-mobile-api.loklok.tv/cms/app`,
+  baseURL: `https://ga-mobile-api.loklok.tv/cms/app`,
   headers: {
     lang: "en",
     versioncode: "11",
